780ac0d717d00f2fef8f23dd175ac6539bb023b7
Garrison Shepard <shepard.garrisont@gmail.com>
2020-11-15 19:43:04 -0500

    connect mongo atlas with pymongo

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 from flask import Flask
+#from flask_mongoengine import MongoEngine
+import pymongo
+
 
 app = Flask(__name__)
+
+
+
+client = pymongo.MongoClient("mongodb+srv://gtshepard:Garrison092894@ehredf.7wanq.mongodb.net/ehredfd4g?retryWrites=true&w=majority")
+db = client.ehredfd4g
+
+
+# grabbed user collection
+print(db.list_collection_names())
+print(db.user.find_one())
+
+
+
+
+#app.config["MONGODB_HOST"] = DB_URI
+# mongodb+srv://garrison:<password>@test.ej3zh.mongodb.net/<dbname>?retryWrites=true&w=majority
 
 @app.route("/")
 def homepage():
@@ -8,7 +27,12 @@ def homepage():
 
 @app.route("/docs")
 def docs():
-    return f'docs page!'
+    names = "NOTHING"
+  
+    
+    
+
+    return f'{names}'
 
 @app.route("/about")
 def about():
