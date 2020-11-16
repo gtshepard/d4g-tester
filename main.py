a9097ee04b933ac1a3011ee890a0a65824a5f8b8
Garrison Shepard <shepard.garrisont@gmail.com>
2020-11-15 19:57:00 -0500

    get users with flask mongo

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 from flask import Flask
-#from flask_mongoengine import MongoEngine
-import pymongo
+from flask_mongoengine import MongoEngine
+#import pymongo
 
 
 app = Flask(__name__)
 
 
+DB_URI = "mongodb+srv://gtshepard:Garrison092894@ehredf.7wanq.mongodb.net/ehredfd4g?retryWrites=true&w=majority"
 
-client = pymongo.MongoClient("mongodb+srv://gtshepard:Garrison092894@ehredf.7wanq.mongodb.net/ehredfd4g?retryWrites=true&w=majority")
-db = client.ehredfd4g
+app.config["MONGODB_HOST"] = DB_URI
+#client = pymongo.MongoClient("mongodb+srv://gtshepard:Garrison092894@ehredf.7wanq.mongodb.net/ehredfd4g?retryWrites=true&w=majority")
+#db = client.ehredfd4g
+db = MongoEngine(app)
 
+class user(db.Document):
+    name = db.StringField(reuired=True, max_length=50)
+    email = db.StringField(reuired=True, max_length=50)
 
-# grabbed user collection
-print(db.list_collection_names())
-print(db.user.find_one())
-
-new_user = {"name": "steve", "email":"steve@gmail.com"}
-db.user.insert(new_user)
-my_query = { "name": "steve" }
-users = db.user.find(my_query)
+users = user.objects()
 for x in users:
-    print(x)
+    print(x.name)
+# grabbed user collection
+#print(db.list_collection_names())
+#print(db.user.find_one())
+
+#new_user = {"name": "steve", "email":"steve@gmail.com"}
+#db.user.insert(new_user)
+#my_query = { "name": "steve" }
+#users = db.user.find(my_query)
+#for x in users:
+#    print(x)
 #app.config["MONGODB_HOST"] = DB_URI
 # mongodb+srv://garrison:<password>@test.ej3zh.mongodb.net/<dbname>?retryWrites=true&w=majority
 
